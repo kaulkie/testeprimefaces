8eb4ca3ac3751adf0c1c2ede14ab9be25a6c9c2b
kaulkie <kaulkie@gmail.com>
2015-02-24 10:49:45 -0500

    Second commit
    Added new view and MBean
    Added javascript import in index.xhtml
    Added development flag in web.xml

---
+++ src/teste/prime/faces/UserView.java
@@ -0,0 +1,26 @@
+package teste.prime.faces;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class UserView {
+
+	private String firstname;
+    private String lastname;
+ 
+    public String getFirstname() {
+        return firstname;
+    }
+ 
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+ 
+    public String getLastname() {
+        return lastname;
+    }
+ 
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
